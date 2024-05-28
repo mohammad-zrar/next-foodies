d435e94bbe8428a81daad56ae9e10501848aa631
mzrar-myrtle <mzrar@myrtle.iq>
2024-05-28 11:07:48 +0300

    Feat(): Improving the loading suspense

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -2,9 +2,16 @@ import classes from './page.module.css'
 import Link from 'next/link'
 import MealsGrid from "../../components/meals/meals-grid"
 import {getMeals} from '../../lib/meals'
-export default async function MealsPage (){
-    const meals = await getMeals();
+import { Suspense } from 'react';
+import LoadingOut from './loading-out'
 
+async function Meals() {
+    const meals = await getMeals();
+    return  <MealsGrid meals={meals}/>
+}
+
+export default function MealsPage (){
+    
     return (
         <>
         <header className={classes.header}>
@@ -17,7 +24,10 @@ export default async function MealsPage (){
             </p>
         </header>
         <main className={classes.main}>
-            <MealsGrid meals={meals}/>
+            <Suspense fallback={<LoadingOut/>}>
+                <Meals/>
+            </Suspense>
+           
         </main>
         </>
     )
