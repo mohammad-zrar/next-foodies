5d48492d2dd440e55def1debed62079e825386d3
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-11-26 23:56:36 +0300

    Feat(components\meals\meals-form-submit.jsx): Fix building error

--- components/meals/meals-form-submit.jsx
+++ components/meals/meals-form-submit.jsx
@@ -2,13 +2,12 @@
 
 import { useFormStatus } from "react-dom";
 
-export default function mealsFormSubmit() {
+export default function MealsFormSubmit() {
   const { pending } = useFormStatus();
+
   return (
-    <>
-      <button disabled={pending}>
-        {pending ? "Submitting" : "Share Meal"}
-      </button>
-    </>
+    <button disabled={pending}>
+      {pending ? "Submitting" : "Share Meal"}
+    </button>
   );
 }
